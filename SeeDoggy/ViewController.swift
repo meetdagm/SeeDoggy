7155e9aaa3178941c1c962385ab41b6fcbae5ec8
Dagmawi Nadew <techoffen4@gmail.com>
2018-10-17 20:41:57 -0400

    Initial Commit

---
+++ SeeDoggy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeDoggy
+//
+//  Created by Dagmawi Nadew-Assefa on 10/17/18.
+//  Copyright © 2018 Sason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
